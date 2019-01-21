1c08d8292989606b9b9bd7d2da839baf0fa04ba1
Scott Dawson <scott@loyaltycorp.com.au>
2019-01-21 14:57:51 +1100

    Catch invalid api response exceptions

--- tests/Exceptions/ExceptionHandlerTest.php
+++ tests/Exceptions/ExceptionHandlerTest.php
@@ -7,17 +7,19 @@ use EoneoPay\ApiFormats\EncoderGuesser;
 use EoneoPay\ApiFormats\External\Libraries\Psr7\Psr7Factory;
 use EoneoPay\Externals\Bridge\Laravel\Translator;
 use EoneoPay\Externals\Environment\Env;
+use EoneoPay\Externals\HttpClient\Exceptions\InvalidApiResponseException;
+use EoneoPay\Externals\HttpClient\Response as ApiResponse;
 use EoneoPay\Framework\Exceptions\EntityNotFoundException;
 use EoneoPay\Framework\Exceptions\ExceptionHandler;
 use EoneoPay\Utils\Exceptions\CriticalException;
 use EoneoPay\Utils\Exceptions\RuntimeException;
+use EoneoPay\Utils\XmlConverter;
 use Exception;
 use Illuminate\Filesystem\Filesystem as ContractedFilesystem;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Translation\Translator as ContractedTranslator;
-use stdClass;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Tests\EoneoPay\Framework\Database\Stubs\EntityStubNotFoundException;
 use Tests\EoneoPay\Framework\Database\Stubs\EntityStubValidationFailedException;
@@ -71,41 +73,6 @@ class ExceptionHandlerTest extends TestCase
     }
 
     /**
-     * Test exception handler always return right response.
-     *
-     * @return void
-     *
-     * @throws \EoneoPay\ApiFormats\Bridge\Laravel\Exceptions\InvalidPsr7FactoryException If psr7 response is invalid
-     * @throws \EoneoPay\Utils\Exceptions\InvalidDateTimeStringException If datetime constructor string is invalid
-     */
-    public function testRender(): void
-    {
-        $exceptionHandler = $this->createExceptionHandler();
-
-        foreach ($this->exceptions as $exception) {
-            $response = $exceptionHandler->render(new Request(), $exception);
-
-            /** @noinspection UnnecessaryAssertionInspection Ensure correct class is returned */
-            self::assertInstanceOf(Response::class, $response);
-        }
-    }
-
-    /**
-     * Create exception handler instance
-     *
-     * @return \EoneoPay\Framework\Exceptions\ExceptionHandler
-     */
-    private function createExceptionHandler(): ExceptionHandler
-    {
-        return new ExceptionHandler(
-            new EncoderGuesser([]),
-            $this->logger,
-            new Psr7Factory(),
-            new Translator(new ContractedTranslator(new FileLoader(new ContractedFilesystem(), __DIR__), 'en'))
-        );
-    }
-
-    /**
      * Test messages don't expose information in production
      *
      * @return void
@@ -119,14 +86,19 @@ class ExceptionHandlerTest extends TestCase
         $request = new Request();
         $exception = new RuntimeExceptionStub('Test message');
 
-        $content = \json_decode($exceptionHandler->render($request, $exception)->content()) ?: new stdClass();
-        self::assertSame('Test message', $content->message ?? 'error');
+        $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+        self::assertSame('Test message', $content['message'] ?? 'error');
 
         // Switch to production and test again
-        (new Env())->set('APP_ENV', 'production');
+        $env = new Env();
+        $environment = $env->get('APP_ENV');
+        $env->set('APP_ENV', 'production');
 
-        $content = \json_decode($exceptionHandler->render($request, $exception)->content()) ?: new stdClass();
-        self::assertSame('exceptions.messages.unknown', $content->message ?? 'error');
+        $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+        self::assertSame('exceptions.messages.unknown', $content['message'] ?? 'error');
+
+        // Reset environment
+        $env->set('APP_ENV', $environment);
     }
 
     /**
@@ -153,8 +125,75 @@ class ExceptionHandlerTest extends TestCase
             $exception = new ClientExceptionStub();
             $exception->setStatusCode($code);
 
-            $content = \json_decode($exceptionHandler->render($request, $exception)->content()) ?: new stdClass();
-            self::assertSame($message, $content->message ?? 'error');
+            $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+            self::assertSame($message, $content['message'] ?? 'error');
+        }
+    }
+
+    /**
+     * Test invalid api response is handled correctly
+     *
+     * @return void
+     *
+     * @throws \EoneoPay\ApiFormats\Bridge\Laravel\Exceptions\InvalidPsr7FactoryException If psr7 response is invalid
+     * @throws \EoneoPay\Utils\Exceptions\InvalidDateTimeStringException If datetime constructor string is invalid
+     * @throws \EoneoPay\Utils\Exceptions\InvalidXmlTagException Inherited, if xml contains an invalid tag
+     */
+    public function testInvalidApiResponseException(): void
+    {
+        $data = [
+            'key' => 'data',
+            'subkey' => [
+                'keya' => 'test',
+                'keyb' => 'test2'
+            ]
+        ];
+
+        $exceptionHandler = $this->createExceptionHandler();
+        $request = new Request();
+
+        // Build an xml response
+        $response = new ApiResponse($data, 400, [], (new XmlConverter())->arrayToXml($data));
+        $exception = new InvalidApiResponseException($response);
+        $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+
+        self::assertIsArray($content);
+        self::assertSame(\array_merge($data, ['@rootNode' => 'data']), $content['message'] ?? []);
+
+        // Build a json response
+        $response = new ApiResponse($data, 400, [], \json_encode($data) ?: '');
+        $exception = new InvalidApiResponseException($response);
+        $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+
+        self::assertIsArray($content);
+        self::assertSame($data, $content['message'] ?? []);
+
+        // Build a standard response
+        $response = new ApiResponse($data, 400, [], 'Testing');
+        $exception = new InvalidApiResponseException($response);
+        $content = \json_decode($exceptionHandler->render($request, $exception)->content(), true) ?: [];
+
+        self::assertIsArray($content);
+        self::assertSame('Testing', $content['message'] ?? []);
+    }
+
+    /**
+     * Test exception handler always return right response.
+     *
+     * @return void
+     *
+     * @throws \EoneoPay\ApiFormats\Bridge\Laravel\Exceptions\InvalidPsr7FactoryException If psr7 response is invalid
+     * @throws \EoneoPay\Utils\Exceptions\InvalidDateTimeStringException If datetime constructor string is invalid
+     */
+    public function testRender(): void
+    {
+        $exceptionHandler = $this->createExceptionHandler();
+
+        foreach ($this->exceptions as $exception) {
+            $response = $exceptionHandler->render(new Request(), $exception);
+
+            /** @noinspection UnnecessaryAssertionInspection Ensure correct class is returned */
+            self::assertInstanceOf(Response::class, $response);
         }
     }
 
@@ -190,5 +229,20 @@ class ExceptionHandlerTest extends TestCase
             /** @noinspection DisconnectedForeachInstructionInspection Fall through if type is unknown */
             self::assertSame('notice', $this->logger->getLogLevel());
         }
+    }
+
+    /**
+     * Create exception handler instance
+     *
+     * @return \EoneoPay\Framework\Exceptions\ExceptionHandler
+     */
+    private function createExceptionHandler(): ExceptionHandler
+    {
+        return new ExceptionHandler(
+            new EncoderGuesser([]),
+            $this->logger,
+            new Psr7Factory(),
+            new Translator(new ContractedTranslator(new FileLoader(new ContractedFilesystem(), __DIR__), 'en'))
+        );
     }
 }
