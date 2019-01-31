91e7940b847fe5248f37175b3c9f2eee51e5a2c2
Nathan Page <nathan.page86@gmail.com>
2019-01-31 16:42:11 +1100

    Make handler abstract, use abstract method to init dontReport, improv… (#22)
    * Make handler abstract, use abstract method to init dontReport, improve encoder usage
    
    * Use a more meaningful method name for the non reportable exceptions

---
+++ tests/Exceptions/Stubs/ExceptionHandlerStub.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\EoneoPay\Framework\Exceptions\Stubs;
+
+use EoneoPay\Framework\Exceptions\ExceptionHandler;
+
+class ExceptionHandlerStub extends ExceptionHandler
+{
+    /**
+     * Initiate the list of exceptions to suppress from the report method
+     *
+     * @return string[]
+     */
+    protected function getNonReportableExceptions(): array
+    {
+        return [];
+    }
+}
