c8a327c47bf1d9eee6cab643d5af1e382b21c1ff
Scott Dawson <scott@loyaltycorp.com.au>
2019-04-09 15:31:13 +1000

    Update externals

---
+++ src/Database/Repositories/Repository.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace EoneoPay\Framework\Database\Repositories;
+
+use EoneoPay\Externals\ORM\Repository as ORMRepository;
+
+class Repository extends ORMRepository
+{
+}
